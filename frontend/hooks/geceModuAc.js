00208b41eb8094cf3e916c74de8ae84eee297a2d
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-04-19 22:32:19 +0300

    Tamamlandı.

--- frontend/hooks/geceModuAc.js
+++ frontend/hooks/geceModuAc.js
@@ -1,6 +1,7 @@
 import localStorageKullan from "./localStorageKullan";
 function useDarkMode() {
   const [geceModu, setGeceModu] = localStorageKullan("geceModu", false);
+  // const [geceModu, setGeceModu]=useState(false); localStorage kullanmadan önce bu şekilde kullanıyorduk.
   // localStorageKullan hook'u kullanılarak, geceModu ve setGeceModu değişkenleri tanımlanır.
   //Burada, localStorageKullan("geceModu", false) ile geceModu değerini localStorage'da saklamış oluyoruz. Eğer daha önce bir değer kaydedilmişse bu değer alınacak, aksi halde varsayılan değer false olacak.
 
